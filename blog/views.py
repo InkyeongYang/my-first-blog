0a1a9f91696ae350413734bb6ad0e7867895f9f4
keiink07 <keiink07@gmail.com>
2019-11-16 16:08:57 +0900

    Creating 1st Django project

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(author=User.objects.get(username='admin'))
+    return render(request, 'blog/post_list.html', {'posts' : posts})
